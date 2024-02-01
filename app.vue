1bc4faefd774c26a9e1fc6e3047b9319ea43803a
龚燎原 <274473435@qq.com>
2024-02-01 15:11:10 +0800

    feat: 修改页面布局

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
-
-import { NuxtPage } from '#build/components';
+import { NuxtLayout } from '#build/components'; import { NuxtPage } from
+'#build/components';
 <!--
  * @Author: 龚燎原 274473435@qq.com
  * @Date: 2024-02-01 01:36:34
  * @LastEditors: 龚燎原
- * @LastEditTime: 2024-02-01 11:25:30
+ * @LastEditTime: 2024-02-01 15:07:27
 -->
 <template>
-  <NuxtPage/>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 <style>
 #jfToolbar,
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: 龚燎原 274473435@qq.com
+ * @Date: 2024-02-01 15:09:21
+ * @LastEditors: 龚燎原
+ * @LastEditTime: 2024-02-01 15:09:44
+-->
+<template>
+    <div class="w-full h-full bg-white dark:bg-slate-900 transition-colors dark:text-slate-300 text-slate-600">
+      <div class="flex flex-col h-fit min-h-screen xl:w-[1200px] m-auto xl:px-2 px-24">
+        <div class="flex-1 flex">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </template>
