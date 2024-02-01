e3641a1a2ca0055d13e6dda4974c0b2a353e409d
龚燎原 <274473435@qq.com>
2024-02-01 15:56:00 +0800

    feat: 增加navbar和footer组件

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+import { Footer } from '#build/components'; import { Footer } from
+'#build/components';
+<!--
+ * @Author: 龚燎原 274473435@qq.com
+ * @Date: 2024-02-01 15:34:15
+ * @LastEditors: 龚燎原
+ * @LastEditTime: 2024-02-01 15:35:25
+-->
+<template>
+  <footer class="h-12 flex justify-center items-center opacity-65 shrink-0">
+    Copyright © 2023-2024, cuixueshe. All rights reserved.
+  </footer>
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: 龚燎原 274473435@qq.com
+ * @Date: 2024-02-01 15:24:29
+ * @LastEditors: 龚燎原
+ * @LastEditTime: 2024-02-01 15:30:01
+-->
+<template>
+<nav class="h-20 flex items-center justify-between shrink-0">
+    <NuxtLink to="/">
+      <div class="logo flex items-center">
+        <img width="48" height="48" class="rounded-md overflow-hidden mr-6" src="/logo.png" alt="earth-worm-logo" />
+        <h1 class="text-2xl leading-none font-black leading-normal text-wrap text-fuchsia-400">
+          Earthworm
+        </h1>
+      </div>
+    </NuxtLink>
+</nav>
+</template>
+
+
+<script setup lang='ts'>
+</script>
+
+
+<style lang='scss' scoped>
+</style>
